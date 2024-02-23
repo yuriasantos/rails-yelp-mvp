603e5e84340837d133c27ad8f613ee2cb8df933f
yuri santos <santosyurial@gmail.com>
2024-02-23 15:22:34 -0300

    Implement all user stories.

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,27 @@
+class ReviewsController < ApplicationController
+  def index
+    @reviews = Review.all
+  end
+
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to @restaurant, notice: 'Review was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,14 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get 'up' => 'rails/health#show', as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # Defines the root path route ('/')
+  # root 'posts#index'
+
+  resources :restaurants, only: %i[index new create show] do
+    resources :reviews, only: %i[new create]
+  end
+
+  root 'restaurants#index'
 end
